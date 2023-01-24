6ec6df4eb8b352869193db06d00ef4292cb1420c
Anderson Santos <andersonguitarman@gmail.com>
2023-01-24 12:25:02 -0300

    Fix in a Ios Platform

--- screens/Styles.js
+++ screens/Styles.js
@@ -1,34 +1,15 @@
 import React from "react";
 import { StyleSheet } from "react-native";
+import { Platform } from "react-native";
 
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-        // marginTop: 50,
-        // marginBottom: 100,
-       
 
     },
     card: {
-        // flex: 1,
-        // // borderWidth: 2,
-        // // borderColor: '#E8E8E8',
-        // // justifyContent: 'center',
-        // backgroundColor: 'white',
-        // borderRadius: 10,
-        // // marginTop: 50,
-        // marginBottom: 50,
-
-        // flex: 1,
-        // borderRadius: 4,
-        // borderWidth: 2,
-        // borderColor: '#E8E8E8',
-        // justifyContent: 'center',
-        // backgroundColor: 'white',
         position: "relative",
         height: "75%",
         borderRadius: 10,
@@ -44,15 +25,11 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-evenly',
         alignItems: 'center',
-        //  width: '80%',
+
         paddingHorizontal: 10,
         marginBottom: 20,
         alignContent: 'center',
-        //buttons on center
-        // marginTop: 50,
-        // marginBottom: 50,
-        // position: "absolute",
-        // bottom: 0,
+
         flex: 0.3,
 
     },
@@ -66,15 +43,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 50,
         marginTop: 50,
-        // marginBottom: 50,
-        //shadow
-        // shadowColor: '#000',
-        // shadowOffset: { width: 0, height: 2 },
-        // shadowOpacity: 0.8,
-        // shadowRadius: 2,
-        // elevation: 1,
-        //how to do a shadow: 0px 10px 16px 
-        // rgba(0, 0, 0, 0.1);
+      
         shadowColor: '#bfbfc0',
         shadowOffset: {
             width: 0,
@@ -90,7 +59,7 @@ const styles = StyleSheet.create({
 
     },
     swithHeader: {
-        marginTop: 60,
+        marginTop: Platform.OS === 'ios' ? 30 : 60,
         height:40,
         alignItems: 'center',
         alignContent: 'center',
@@ -103,15 +72,12 @@ const styles = StyleSheet.create({
     textNumber: {
         fontSize: 16,
         fontFamily: 'NunitoSans_700Bold',
-        //fontWeight 700
+  
     
         fontStyle: 'normal',
-        // lineHeight: '125%',
+  
         color: '#434141',
-        //to keep the text on the right
-    
-
-      
+  
         justifyContent: "flex-end",
         alignSelf: "flex-end",
        
@@ -120,26 +86,16 @@ const styles = StyleSheet.create({
     },
     textName: {
         fontSize: 16,
-        fontFamily: 'NunitoSans_700Bold',
-        //fontWeight 700
-    
-        fontStyle: 'normal',
-        // lineHeight: '125%',
+        fontFamily: 'NunitoSans_700Bold',    
+        fontStyle: 'normal',  
         color: '#434141',
-        //to keep the text on the right
-    
-
-       
-
 
     },
     textCountry: {
         fontSize: 8,
         fontFamily: 'NunitoSans_700Bold',
-        //fontWeight 700
       
         fontStyle: 'normal',
-        // lineHeight: '125%',
         color: '#BFBFC0',
 
     },
